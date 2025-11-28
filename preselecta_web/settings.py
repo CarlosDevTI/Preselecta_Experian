8a4c34e578090a56f2b2c43b15cfbbcc6d33b720
CarlosDevTI <analistati.ingenieria@gmail.com>
2025-11-28 12:50:26 -0500

    Solución para logo y favicon en gunicorn

--- preselecta_web/settings.py
+++ preselecta_web/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,6 +166,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
