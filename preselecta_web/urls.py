466ee333d77ec47696896c102775d0be2ad95f26
CarlosDevTI <analistati.ingenieria@gmail.com>
2026-02-22 19:51:11 -0500

    fix: Ajuste en la visualizacion de media con nginx

--- preselecta_web/urls.py
+++ preselecta_web/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for preselecta_web project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve as static_serve
 
 
 urlpatterns = [
@@ -29,6 +14,11 @@ urlpatterns = [
     path('preselecta/', include("integrations.urls")),
 ]
 
-# Este despliegue usa Gunicorn directo sin Nginx dedicado para media.
-# Por eso exponemos /media tambien en produccion.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Produccion sin Nginx dedicado para media:
+    # exponemos /media de forma explicita con Django.
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", static_serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
